2dd64d9ef95a8a17c2e8281785ed58cac82d0175
ynubhav <anubhavdixit35@gmail.com>
2025-11-15 01:51:59 +0530

    refactor Route schema: change projectId type to String, add PATCH method, and set default description

--- backend/src/models/Route.model.js
+++ backend/src/models/Route.model.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
 const RouteSchema = new mongoose.Schema({
-  projectId: { type: mongoose.Schema.Types.ObjectId, ref: "APIProject", required: true },
+  projectId: { type:String,required: true },
   path: { type: String, required: true }, // e.g., /status/:id/items
-  method: { type: String, enum: ["GET", "POST", "PUT", "DELETE"], required: true },
+  method: { type: String, enum: ["GET", "POST", "PUT", "DELETE","PATCH"], required: true },
 
   // Controls
   authRequired: { type: Boolean, default: true },
   cacheEnabled: { type: Boolean, default: false },
   cacheTTL: { type: Number, default: 60 }, // in seconds
 
-  description: { type: String },
+  description: { type: String ,default:""},
 });
 const Route=mongoose.model("Route", RouteSchema);
 
