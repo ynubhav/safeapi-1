de0a82d1d19cd24ad0f36e730a99c3da28c1ee4d
ynubhav <anubhavdixit35@gmail.com>
2025-11-15 23:45:35 +0530

    feat: enhance gateway route with route validation and logging functionality

--- backend/src/routes/gateway.route.js
+++ backend/src/routes/gateway.route.js
@@ -3,10 +3,14 @@ import axios from "axios";
 import https from "https";
 import dns from "dns";
 import { APIProject } from "../models/Project.model.js";
+import { APIKey } from "../models/Apikey.model.js";
+import { RequestLog } from "../models/Apilogs.model.js";
+import { Route } from "../models/Route.model.js";
+import { matchRoute } from "../utils/pathMatcher.js";
 
 const gatewayRouter = express.Router();
 
-// 🧠 agent to ignore SSL issues (only temporary for dev)
+// agent to ignore SSL issues (only temporary for dev)
 const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
 gatewayRouter.use("/:projectId", async (req, res) => {
@@ -15,11 +19,14 @@ gatewayRouter.use("/:projectId", async (req, res) => {
   const method = req.method.toUpperCase();
   const apiKey = req.get("x-safeapi-key");
   const origin = req.get("origin");
-
+  const urlwithquery = req.url;
+  const incommingurl = urlwithquery.split("?")[0];
+  const initTime0 = Date.now();
+  console.log(incommingurl, urlwithquery);
   try {
     // validate project
     const project = await APIProject.findOne({ projectId }).select(
-      "apiKey originUrl status allowedOrigins publicRoutes PrivateRoutes"
+      "originUrl status allowedOrigins"
     );
     if (!project) {
       return res.status(404).json({ error: "Project not found" });
@@ -31,22 +38,56 @@ gatewayRouter.use("/:projectId", async (req, res) => {
     }
 
     console.log(1);
+    const initTime1 = Date.now();
     // authorize
-    if (project.apiKey != apiKey) {
-      return res.status(403).json({ error: "Invalid API key" });
+    const isApikey = await APIKey.findOneAndUpdate(
+      { projectId, key: apiKey, keystatus: "active" },
+      { lastUsed: Date.now() }
+    );
+    if (!isApikey) {
+      return res.status(403).json({ message: "Invalid/Disabled Apikey" });
     }
+
     console.log(2);
-    // optional: restrict who can call the gateway
-    if (origin && project.originUrl && !origin.startsWith(project.originUrl)) {
-      return res.status(403).json({ error: "Unauthorized origin" });
+    //match and check if route valid and return the path
+    const initTime2 = Date.now();
+    let isvalidRoute = false;
+    let urlpath = "";
+
+    const initialCheck = await Route.findOne({
+      method,
+      path: incommingurl,
+      projectId,
+    });
+    const initTime3 = Date.now();
+    if (!initialCheck) {
+      const allroutes = await Route.find({ method, projectId });
+
+      for (let i = 0; i < allroutes.length; i++) {
+        if (matchRoute(allroutes[i].path, incommingurl).matched) {
+          urlpath = allroutes[i].path;
+          isvalidRoute = true;
+          break;
+        }
+      }
+    } else {
+      isvalidRoute = true;
+      urlpath = initialCheck.path;
     }
+    const initTime4 = Date.now();
+    if (!isvalidRoute) {
+      return res.status(404).json({ message: "Invalid Route" });
+    }
+    // console.log(RouteInfo);
+
     console.log(3);
     // redirect only if active status
     if (project.status === "suspended") {
       return res.status(503).json({ message: "API Status : Suspended" });
     }
     // prepare redirect target
-    const redirectUrl = `${project.originUrl}${req.url}`;
+    const redirectUrl = `${project.originUrl}${urlwithquery}`;
+    const initTime5 = Date.now();
     console.log(redirectUrl);
     const parsed = new URL(redirectUrl);
     if (parsed.protocol !== "https:") {
@@ -61,6 +102,7 @@ gatewayRouter.use("/:projectId", async (req, res) => {
       });
     });
     console.log(5);
+    const initTime6 = Date.now();
     // build axios config
     const config = {
       method,
@@ -79,17 +121,40 @@ gatewayRouter.use("/:projectId", async (req, res) => {
     }
     console.log(7);
     // send request
+
     const response = await axios(config);
     console.log(8);
     // forward response
+    const timeb4req = Date.now();
     res.status(response.status).set(response.headers).send(response.data);
-
+    const finalTime = Date.now();
+    //console.log(9);
+    console.log("time taken by user's api :", finalTime - timeb4req);
+    console.log(
+      initTime0 - initTime,
+      initTime1 - initTime,
+      initTime2 - initTime1,
+      initTime3 - initTime2,
+      initTime4 - initTime3,
+      initTime5 - initTime4,
+      initTime6 - initTime5,
+      timeb4req - initTime6
+    );
     // 🪵 future steps:
     // - log the request in Mongo
+    const MakeRequestLog = await RequestLog.create({
+      projectId,
+      keyId: isApikey._id,
+      route: urlpath,
+      method,
+      origin,
+      statusCode: response.status,
+      latency: finalTime - initTime,
+      user_api_latency: finalTime - timeb4req,
+    });
     // - store cache in Redis if cacheable
     // - increment usage counter
-    const finalTime = Date.now();
-    console.log(finalTime - initTime);
+    // console.log(MakeRequestLog);
   } catch (err) {
     console.error("Gateway error:", err.message);
     res.status(502).json({
