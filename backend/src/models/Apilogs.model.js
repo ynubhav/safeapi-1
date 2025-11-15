08898b310ad91f9b48632f13150af1151b018772
ynubhav <anubhavdixit35@gmail.com>
2025-11-15 23:45:10 +0530

    refactor: change projectId type to String in RequestLog schema

--- backend/src/models/Apilogs.model.js
+++ backend/src/models/Apilogs.model.js
@@ -1,19 +1,20 @@
 import mongoose from "mongoose";
 
 const RequestLogSchema = new mongoose.Schema({
-  projectId: { type: mongoose.Schema.Types.ObjectId, ref: "APIProject", required: true },
-
+  projectId: { type: String, required: true },
+  keyId: { type: mongoose.Schema.Types.ObjectId, ref: "APIKey" },
   route: { type: String, required: true }, // route ex : project/:id/...
   method: { type: String, required: true },
   origin: { type: String },
 
   statusCode: { type: Number },
+  user_api_latency: { type: Number },
   latency: { type: Number }, // in ms
   cached: { type: Boolean, default: false },
 
   timestamp: { type: Date, default: Date.now },
 });
 
-const RequestLog= mongoose.model("RequestLog", RequestLogSchema);
+const RequestLog = mongoose.model("RequestLog", RequestLogSchema);
 
-export {  RequestLog };
+export { RequestLog };
