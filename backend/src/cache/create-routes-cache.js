c7779ca5bace8760f494a802f7adde0a27f7449a
ynubhav <anubhavdixit35@gmail.com>
2025-11-17 23:44:35 +0530

    feat: add create_routes_cache function to initialize routes cache from database

---
+++ backend/src/cache/create-routes-cache.js
@@ -0,0 +1,30 @@
+import { Route } from "../models/Route.model.js";
+import { Routes_cache } from "./routes-cache.js";
+
+const create_routes_cache = async () => {
+  try {
+    const loadroutes = await Route.find({});
+    for (let i = 0; i < loadroutes.length; i++) {
+      const route = loadroutes[i];
+      if (!Routes_cache[route.projectId]) {
+        Routes_cache[route.projectId] = {};
+      }
+      if (!Routes_cache[route.projectId][route.method]) {
+        Routes_cache[route.projectId][route.method] = [];
+      }
+      Routes_cache[route.projectId][route.method].push({
+        path: route.path,
+        authRequired: route.authRequired,
+        cacheEnabled: route.cacheEnabled,
+        cacheTTL: route.cacheTTL,
+        description: route.description,
+      });
+    }
+    Routes_cache.__meta = { updatedAt: Date.now() };
+    console.log("ROUTES CACHE CREATION SUCCCESSFULL ✅");
+  } catch (error) {
+    console.log("ERROR CREATING ROUTES CACHE");
+  }
+};
+
+export default create_routes_cache;
